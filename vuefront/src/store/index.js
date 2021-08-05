8a81cf6b03509606ada47e7abbb79f1eae7ea28b
dev-Lesser <dev.jongmoon@gmail.com>
2021-08-05 10:00:00 +0900

    :art: Edit Vue frontend file

--- vuefront/src/store/index.js
+++ vuefront/src/store/index.js
@@ -4,8 +4,8 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-    showNav: true,
-    items: null
-  },
+    state: {
+        showNav: true,
+        items: null
+    },
 });
